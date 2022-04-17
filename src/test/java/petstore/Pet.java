e15f66a8fd247f77e290d233fb6399032fd88c46
carlosoliveira1 <carlosoliveiramauricio@gmail.com>
2022-04-17 20:58:17 -0300

    Finalizado o metodo Pet

--- src/test/java/petstore/Pet.java
+++ src/test/java/petstore/Pet.java
@@ -21,7 +21,7 @@ public class Pet {
         return new String(Files.readAllBytes(Paths.get(caminhoJson)));
     }
 
-    @Test
+    @Test(priority = 1)
     public void  incluirPet() throws IOException {
 
         String jsonBody = lerJson("db/pat1.json");
@@ -34,7 +34,7 @@ public class Pet {
         .then()
                 .log().all()
                 .statusCode(200)
-                .body("name", is("doggie"))
+                .body("name", is("XPTO0123"))
                 .body("status", is("available"))
                 //.body("category.name", is("dog"))//Deveria estar passando "dog", mas está gerando erro
                 //.body("tags.name", contains("dog"))
@@ -42,10 +42,11 @@ public class Pet {
 
     }
 
-    @Test
+    @Test(priority = 2)
     public  void consultarPet(){
-        String petId = "9222968140497182000";
+        String petId = "9223372036854775807";
 
+        String token =
         given()
                 .contentType("application/json")
                 .log().all()
@@ -54,10 +55,53 @@ public class Pet {
 
         .then().log().all()
                 .statusCode(200)
-                .body("name", is("doggie"))
+                .body("name", is("XPTO0123"))
                // .body("category.name", is("dog"))
                 .body("status", is("available"))
+        .extract()
+                .path("category.name")
+
+
         ;
+        System.out.println("O token é " + token);
+    }
+
+    @Test(priority = 3)
+    public  void alterarPet() throws IOException {
+        String jsonBody = lerJson("db/pat2.json");
+
+        given()
+                .contentType("application/json")
+                .log().all()
+                .body(jsonBody)
+        .when()
+                .put(uri)
+        .then()
+                .log().all()
+                .statusCode(200)
+                .body("name", is("XPTO01231"))
+                .body("status", is("sold"))
+        ;
+
+    }
+
+    @Test(priority = 4)
+    public void excluirPet(){
+        String petId = "9223372036854775807";
+
+        given()
+                .contentType("application/json")
+                .log().all()
+        .when()
+                .delete(uri + "/" + petId)
+        .then()
+                .log().all()
+                .statusCode(200)
+                .body("code", is(200))
+                .body("type", is("unknown"))
+                .body("message", is(petId))
+        ;
+
     }
 
 }
